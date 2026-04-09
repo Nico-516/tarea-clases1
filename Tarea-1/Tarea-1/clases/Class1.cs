d7b805b2edcbf3207e13b0c7fc45345e83199f2e
Nico-516 <Nicolas.G.Sanchez.516@gmail.com>
2026-04-09 02:07:12 -0300

    Project reset from home

---
+++ Tarea-1/Tarea-1/clases/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Persona
+{
+	string _nombre { get; }
+	string _email { get; }
+	
+	public Persona(string nombre, string email)
+	{
+		_email = email;
+		_nombre = nombre;
+	}
+
+	public static virtual void presentarse()
+	{
+		Console.WriteLine($"Hola, soy {_nombre}");
+	}
+
+	public class Alumno : Persona
+	{
+		int _legajo { get; }
+
+		list<string> _materias = new list<string>();
+
+		public Alumno(int legajo)
+		{
+			_legajo = legajo;
+		}
+
+		public static override void presentarse()
+		{
+			Console.Writeline($"Alumno {_nombre}, legajo {_legajo}");
+		}
+
+		public static void inscribirse(int cantMaterias)
+		{
+			for (int i = 0; i < cantMaterias+1; i++)
+			{
+				string nuevaMateria = Console.Readline($"Ingrese el nombre de la materia {i+1}: ");
+				_materias.Add(nuevaMateria);
+			}
+		}
+    }
+}
